7d91efef752e658891cd1229f32b51d02aa46ff3
ayush <ayush.mehta010906@gmail.com>
2025-07-25 12:54:37 +0000

    completed the 6th exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let result = '';
+  for (let i = 0; i < num; i++) {
+    result += string;
+  }
+  return result;
 };
+
+console.log(repeatString('hey', 3)); 
+
 
 // Do not edit below this line
 module.exports = repeatString;
