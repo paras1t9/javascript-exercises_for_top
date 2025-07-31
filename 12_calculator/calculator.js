e5aef8f1ada1844d3ed59113af236d6372c4a36d
ayush <ayush.mehta010906@gmail.com>
2025-07-31 12:45:28 +0000

    completed the 12th exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	for (let element of arr){
+    sum += element;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  for (let num of arr){
+    product *= num;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let result = 1
+  for (let count = 0; count < b; count++){
+    result *= a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(int) {
+	let num = 1;
+  if (int === 0){
+    return num;
+  }else{
+    for(let digit = int; digit > 0; digit--){
+      num *= digit;
+    }
+  }
+  return num;
 };
 
 // Do not edit below this line
