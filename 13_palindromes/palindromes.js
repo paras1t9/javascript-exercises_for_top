e5be8b2e1729fe7a65a1148e390f47bc04b2165c
ayush <ayush.mehta010906@gmail.com>
2025-07-31 13:22:51 +0000

    completed the 13th exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+  const original = string
+    .toLowerCase()
+    .split('')
+    .filter((character) => characters.includes(character))
+    .join('');
+
+  const reversed = original.split('').reverse().join('');
+
+  return original === reversed;
 };
 
-// Do not edit below this line
 module.exports = palindromes;
